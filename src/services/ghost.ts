e260b58a32c9a603d2a83beab25ed31ba69187cd
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-06-15 11:32:41 -0700

    implement support for fetching a Ghost member

--- src/services/ghost.ts
+++ src/services/ghost.ts
@@ -1,5 +1,14 @@
 import path from 'node:path';
-import {ghostUrl} from './config.js';
+import GhostAdminApi from '@tryghost/admin-api';
+import {GhostMemberWithTiers} from '../types/ghost.js';
+import {isObject} from '../lib/is-object.js';
+import {ghostUrl, ghostApiKey} from './config.js';
+
+const adminApi = new GhostAdminApi({
+	url: ghostUrl,
+	key: ghostApiKey,
+	version: 'v5.0',
+});
 
 export function getGhostUrl(urlPath: string, hash = ''): string {
 	const base = new URL(ghostUrl);
@@ -7,4 +16,16 @@ export function getGhostUrl(urlPath: string, hash = ''): string {
 	base.hash = hash;
 
 	return base.toString();
+}
+
+export async function getMember(id: string): Promise<GhostMemberWithTiers | false> {
+	try {
+		return await adminApi.members.read({id}, {include: 'tiers'}) as unknown as GhostMemberWithTiers;
+	} catch (error: unknown) {
+		if (isObject(error) && error.type === 'NotFoundError') {
+			return false;
+		}
+
+		throw error;
+	}
 }
