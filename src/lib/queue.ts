e4d9ee596595ae813fc48ea62fd2e5edc36d305d
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-06-15 19:47:01 -0700

    add basic queue

---
+++ src/lib/queue.ts
@@ -0,0 +1,38 @@
+import logging from '@tryghost/logging';
+import {Semaphore} from './semaphore.js';
+
+export class Queue {
+	private readonly _semaphore: Semaphore;
+	private readonly _jobs = new Set<string>();
+	private _jobId = 0;
+
+	constructor(delay = 1000) {
+		this._semaphore = new Semaphore(1, delay);
+	}
+
+	async add<Func extends (...args: any[]) => any>(
+		name: string, func: Func, ...args: Parameters<Func>
+	): Promise<ReturnType<Func>> {
+		this._jobs.add(name);
+		this._jobId++;
+		const unsubscribe = await this._semaphore.acquire();
+		logging.info(`Running job ${this._jobId} (${func.name ?? '<anonymous>'}). ${this._jobs.size} jobs in queue.`);
+
+		try {
+			// We want to immediately remove the job from the list of jobs in case
+			// something happens that causes the job to be re-queued. (e.g. an event)
+			this._jobs.delete(name);
+			return await Promise.resolve(func(...args)); // eslint-disable-line @typescript-eslint/no-unsafe-return
+		} finally {
+			unsubscribe();
+		}
+	}
+
+	has(name: string) {
+		return this._jobs.has(name);
+	}
+
+	get count() {
+		return this._jobs;
+	}
+}
