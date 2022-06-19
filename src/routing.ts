28bf8c3116d97e7fadc88eee52ebdb3c7f7ced5e
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-06-18 18:10:26 -0700

    refactor routing manager to use DI

--- src/routing.ts
+++ src/routing.ts
@@ -2,17 +2,9 @@ import path from 'node:path';
 import {Application, NextFunction, Request, Response, json} from 'express';
 import logging from '@tryghost/logging';
 import {config} from './services/config.js';
-import {ssoController} from './controllers/sso.js';
-import {ghostWebhookController} from './controllers/ghost-webhook.js';
-
-const {
-	mountedBasePath, enableGhostWebhooks, ghostMemberDeletedRoute, ghostMemberUpdatedRoute, ghostMemberDeleteDiscourseAction: deleteAction,
-	ssoMethod,
-} = config;
-
-export function route(routePath: string): string {
-	return path.resolve(mountedBasePath, routePath.replace(/^\//, ''));
-}
+import {SSOController, ssoController} from './controllers/sso.js';
+import {GhostWebhookController, ghostWebhookController} from './controllers/ghost-webhook.js';
+import {Configuration} from './types/config.js';
 
 function lazyJson(payload: Record<string, unknown>, statusCode = 200) {
 	return (_: Request, response: Response) => {
@@ -24,16 +16,47 @@ function lazyJson(payload: Record<string, unknown>, statusCode = 200) {
 	};
 }
 
-export function addRoutes(app: Application, includeCommon = false): void {
-	app.get(route('sso'), ssoController.controllerFor(ssoMethod));
+export class RoutingManager {
+	constructor(
+		private readonly config: Configuration,
+		private readonly ghostWebhookController: GhostWebhookController,
+		private readonly sso: SSOController,
+	) {}
 
-	if (enableGhostWebhooks) {
-		const fullMemberUpdatedRoute = route(`hook/${ghostMemberUpdatedRoute}`);
-		const fullMemberDeletedRoute = route(`hook/${ghostMemberDeletedRoute}`);
-		const deleteHandler = ghostWebhookController.deleteController(deleteAction);
+	resolve(route: string) {
+		return path.resolve(this.config.mountedBasePath, route.replace(/^\//, ''));
+	}
+
+	addCoreRoutes(app: Application) {
+		app.get(this.resolve('sso'), this.sso.controllerFor(this.config.ssoMethod));
+
+		if (this.config.enableGhostWebhooks) {
+			this.addGhostWebhookRoutes(app);
+		}
+	}
+
+	addAllRoutes(app: Application) {
+		this.addCoreRoutes(app);
+		const sendError = lazyJson({error: 'Something went wrong. Unable to complete request.'}, 500);
+		app.get(this.resolve('/health'), lazyJson({message: 'Howdy!'}));
+		app.use(lazyJson({error: 'Not Found'}, 404));
+		app.use((error: unknown, request: Request, response: Response, ___: NextFunction) => {
+			logging.error(error);
+			sendError(request, response);
+		});
+	}
+
+	private addGhostWebhookRoutes(app: Application) {
+		const {
+			ghostMemberDeletedRoute, ghostMemberUpdatedRoute, ghostMemberDeleteDiscourseAction: deleteAction,
+		} = this.config;
+
+		const fullMemberUpdatedRoute = this.resolve(`hook/${ghostMemberUpdatedRoute}`);
+		const fullMemberDeletedRoute = this.resolve(`hook/${ghostMemberDeletedRoute}`);
+		const deleteHandler = this.ghostWebhookController.deleteController(deleteAction);
 		const jsonParser = json();
 
-		app.post(fullMemberUpdatedRoute, jsonParser, ghostWebhookController.memberUpdated);
+		app.post(fullMemberUpdatedRoute, jsonParser, this.ghostWebhookController.memberUpdated);
 
 		if (deleteHandler) {
 			app.post(fullMemberDeletedRoute, jsonParser, deleteHandler);
@@ -45,14 +68,6 @@ export function addRoutes(app: Application, includeCommon = false): void {
 			+ (deleteHandler ? '\n - Member Deleted @ ' + fullMemberDeletedRoute : ''),
 		);
 	}
-
-	if (includeCommon) {
-		const sendError = lazyJson({error: 'Something went wrong. Unable to complete request.'}, 500);
-		app.get(route('/health'), lazyJson({message: 'Howdy!'}));
-		app.use(lazyJson({error: 'Not Found'}, 404));
-		app.use((error: unknown, request: Request, response: Response, ___: NextFunction) => {
-			logging.error(error);
-			sendError(request, response);
-		});
-	}
 }
+
+export const routingManager = new RoutingManager(config, ghostWebhookController, ssoController);
