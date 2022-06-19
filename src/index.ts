4929787b7a52219a0324eef9bff33199bf2a6cd9
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-06-18 18:10:26 -0700

    refactor routing manager to use DI

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import logging from '@tryghost/logging';
 import {config} from './services/config.js';
 import {logRequest} from './controllers/middleware.js';
-import {addRoutes} from './routing.js';
+import {routingManager} from './routing.js';
 
 export const app = express();
 
 app.use(logRequest);
-addRoutes(app, true);
+routingManager.addAllRoutes(app);
 
 app.listen(config.port, config.hostname, () => {
 	logging.info(`Listening on http://${config.hostname}:${config.port}`);
