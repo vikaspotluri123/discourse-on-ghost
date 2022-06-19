cec950cd4ca86f8887266c49e02bef9f56538174
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-06-18 17:38:03 -0700

    refactor memberSyncService to use DependencyInjection

--- src/controllers/ghost-webhook.ts
+++ src/controllers/ghost-webhook.ts
@@ -2,12 +2,12 @@ import {NextFunction, Request, Response} from 'express';
 import logging from '@tryghost/logging';
 import errors from '@tryghost/errors';
 import {MemberRemoved, MemberUpdated} from '../types/ghost.js';
-import {memberSyncQueue, syncMemberGroups, setDiscourseGroupsFromGhostTiers as setGroups} from '../services/member-sync.js';
+import {memberSyncService} from '../services/member-sync.js';
 import {discourseService as ds} from '../services/discourse.js';
 
 type SafeMemberRemoved = MemberRemoved['member']['previous'] & {id: string};
 
-const wrapRemoveOption = (handle: (payload: SafeMemberRemoved) => Parameters<typeof memberSyncQueue['add']>) =>
+const wrapRemoveOption = (handle: (payload: SafeMemberRemoved) => Parameters<typeof memberSyncService.queue['add']>) =>
 	(request: Request, response: Response, next: NextFunction) => {
 		logging.info('Processing member removed event');
 		const body: MemberRemoved = request.body as unknown as MemberRemoved;
@@ -17,7 +17,7 @@ const wrapRemoveOption = (handle: (payload: SafeMemberRemoved) => Parameters<typ
 			return;
 		}
 
-		void memberSyncQueue.add(...handle(body.member.previous as SafeMemberRemoved));
+		void memberSyncService.queue.add(...handle(body.member.previous as SafeMemberRemoved));
 		response.status(202).json({message: 'Syncing member'});
 	};
 
@@ -31,22 +31,24 @@ export function memberUpdated(request: Request, response: Response, next: NextFu
 	}
 
 	const {id, uuid, tiers} = body.member.current;
-	if (memberSyncQueue.has(id)) {
+	if (memberSyncService.queue.has(id)) {
 		response.status(202).json({message: 'Syncing member'});
 		return;
 	}
 
 	if (!tiers) {
-		void memberSyncQueue.add(id, syncMemberGroups, id);
+		void memberSyncService.queue.add(id, memberSyncService.syncGroups, id);
 		response.status(202).json({message: 'Syncing member'});
 		return;
 	}
 
-	void memberSyncQueue.add(id, setGroups, uuid, tiers);
+	void memberSyncService.queue.add(id, memberSyncService.setDiscourseGroupsFromTiers, uuid, tiers);
 	response.status(202).json({message: 'Syncing member'});
 }
 
-export const memberRemovedSync = wrapRemoveOption(member => [member.id, setGroups, member.uuid, []]);
+export const memberRemovedSync = wrapRemoveOption(member =>
+	[member.id, memberSyncService.setDiscourseGroupsFromTiers, member.uuid, []],
+);
 export const memberRemovedSuspend = wrapRemoveOption(member => [member.id, ds.suspendExternalUser, member.uuid]);
 export const memberRemovedAnonymize = wrapRemoveOption(member => [member.id, ds.anonymizeExternalUser, member.uuid]);
 export const memberRemovedDelete = wrapRemoveOption(member => [member.id, ds.deleteExternalUser, member.uuid]);
