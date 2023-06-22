53cef6a6c9740cf2b23b9b05403277d19fd3b8d5
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-06-21 20:14:56 -0700

    add test command

--- scripts/dev.js
+++ scripts/dev.js
@@ -6,6 +6,8 @@ import glob from 'glob';
 import {build} from 'esbuild';
 import nodemon from 'nodemon';
 
+const disableWatch = 'NO_WATCH' in process.env;
+
 await build({
 	entryPoints: glob.sync('./src/**/*.ts'),
 	target: 'node16',
@@ -14,6 +16,10 @@ await build({
 	outdir: './dist',
 	treeShaking: true,
 });
+
+if (disableWatch) {
+	process.exit();
+}
 
 const watcher = nodemon({
 	script: 'dist/index.js',
